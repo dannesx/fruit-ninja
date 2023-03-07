b89419bc59ae30bd2504a4e5636144e688e1a1be
Daniel Antunes <daniel.viantunes@gmail.com>
2023-03-07 13:14:10 -0300

    Importing assets and Spawner

---
+++ Scripts/Spawner.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private Collider spawnArea;
+
+    public GameObject[] fruitPrefabs;
+
+    [Header("Spawn Delay")]
+    public float minSpawnDelay = 0.25f;
+    public float maxSpawnDelay = 1f;
+
+    [Header("Angle")]
+    public float minAngle = -15f;
+    public float maxAngle = 15f;
+
+    [Header("Force")]
+    public float minForce = 18f;
+    public float maxForce = 22f;
+
+    [Header("Lifetime")]
+    public float maxLifetime = 5f;
+
+    private void Awake(){
+        spawnArea = GetComponent<Collider>();
+    }
+
+    private void OnEnable(){
+        StartCoroutine(Spawn());
+    }
+
+    private void OnDisable(){
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Spawn(){
+        yield return new WaitForSeconds(2f);
+
+        while(enabled){
+            GameObject prefab = fruitPrefabs[Random.Range(0, fruitPrefabs.Length)];
+
+            Vector3 position = new Vector3();
+            position.x = Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x);
+            position.y = Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y);
+            position.z = Random.Range(spawnArea.bounds.min.z, spawnArea.bounds.max.z);
+
+            Quaternion rotation = Quaternion.Euler(0f, 0f, Random.Range(minAngle, maxAngle));
+
+            GameObject fruit = Instantiate(prefab, position, rotation);
+            Destroy(fruit, maxLifetime);
+
+            float force = Random.Range(minForce, maxForce);
+            fruit.GetComponent<Rigidbody>().AddForce(fruit.transform.up * force, ForceMode.Impulse);
+
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+        }
+    }
+}
