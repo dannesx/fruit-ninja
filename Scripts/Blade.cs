0fcdfeb1b0d5b9dc0ba68ba866eeeade20ae9571
Daniel Antunes <71462802+dannesx@users.noreply.github.com>
2023-03-14 12:35:06 -0300

    Blade

---
+++ Scripts/Blade.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Collider bladeCollider;
+    private TrailRenderer bladeTrail;
+    private bool slicing;
+
+    public Vector3 direction { get; private set; }
+    public float minSliceVelocity = 0.01f;
+
+    private void Awake() {
+        mainCamera = Camera.main;
+        bladeCollider = GetComponent<Collider>();
+        bladeTrail = GetComponentInChildren<TrailRenderer>();
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)){
+            slicing = true;
+            bladeTrail.Clear();
+        } else if (Input.GetMouseButtonUp(0)){
+            slicing = false;
+        }
+        
+        bladeCollider.enabled = slicing;
+        bladeTrail.enabled = slicing;
+        Slicing();
+    }
+
+    private void Slicing(){
+        if(!slicing) return;
+
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        direction = newPosition - transform.position;
+
+        float velocity = direction.magnitude / Time.deltaTime;
+        bladeCollider.enabled = velocity > minSliceVelocity;
+
+        transform.position = newPosition;
+    }
+}
